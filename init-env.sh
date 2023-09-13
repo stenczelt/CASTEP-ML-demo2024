a2cf1625bd536b3b50d21c256819920bbc076892
T. K. Stenczel <tks32@cam.ac.uk>
2023-09-13 10:10:39 +0100

    add command alias

--- init-env.sh
+++ init-env.sh
@@ -1,4 +1,5 @@
 # init the environment
+alias ll="ls -ltr --color=auto"
 module load netlib-scalapack fftw python-data
 export PATH="/projappl/project_2008476/tks32/bin:$PATH"
 python -m virtualenv venv
